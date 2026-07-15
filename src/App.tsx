9fb64d37c76fb9ea59835fb4f56a5abcec33ff42
Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>
2026-07-15 10:04:03 +0200

    fix: hour format
    Signed-off-by: Leonard Sheng Sheng Lee <305414+sheeeng@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ export default function App() {
 
   // Formatting time gracefully adapting to the user's local timezone & locale.
   const formatParts = new Intl.DateTimeFormat(undefined, {
-    hour: 'numeric',
+    hour: '2-digit',
     minute: '2-digit',
     second: '2-digit'
   }).formatToParts(time);
